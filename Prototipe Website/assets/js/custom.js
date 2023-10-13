3932badd272351d94f98bed13af51df0cd43a692
Alvin Pradana Antony <quickgaming321@gmail.com>
2023-10-13 10:48:55 +0700

    Add: Fake Login, Upload file btn

--- Prototipe Website/assets/js/custom.js
+++ Prototipe Website/assets/js/custom.js
@@ -23,3 +23,49 @@ $(".show-hide").click(function () {
     $("#iconShowHide").css("color", "var(--text-color-light)");
   }
 });
+
+let buttonLogout = document.getElementById("buttonLogout");
+let textUsername = document.getElementById("textUsername");
+let textPassword = document.getElementById("textPassword");
+let showContent = document.getElementById("showContent");
+
+function termscheck() {
+  terms.checked = true;
+}
+
+// Sign-In
+function buttonSubmit() {
+  let nipInput = document.getElementById("nipInput");
+  let passInput = document.getElementById("passInput");
+  localStorage.setItem("nip", nipInput.value);
+
+  if (nipInput.value == "superadmin" && passInput.value == "superadmin") {
+    localStorage.setItem("role", "admin");
+    window.location.href = "faq.html";
+  } else if (nipInput.value == "pegawai" && passInput.value == "pegawai") {
+    localStorage.setItem("role", "basic");
+  } else if (nipInput.value == "" || passInput.value == "") {
+    alert("Tolong Masukan NIP Dan Kata Sandi Anda");
+  } else {
+    alert("NIP Atau Kata Sandi Anda Salah");
+  }
+}
+
+if (localStorage.getItem("nip")) {
+  if (localStorage.getItem("role") == "admin") {
+    window.location.href = "faq.html";
+    //    showContent.textContent = '"Superadmin"';
+    // showContent.style.display = "block";
+  }  else if (localStorage.getItem("role") == "basic") {
+    if (nipInput.value == "pegawai") {
+      // showContent.textContent = "Pegawai 1";
+      // showContent.style.display = "block";
+    }
+  }
+}
+
+// logout
+function logout() {
+  localStorage.clear();
+  location.reload();
+}
