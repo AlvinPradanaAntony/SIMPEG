671bbb65c1fe0bea2cba71b0db4ad922574ceb22
Developer <quickgaming321@gmail.com>
2023-12-03 02:07:48 +0700

    Add faq seeder for data testing

---
+++ helpdesk_website/resources/js/Components/SearchFilter.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+defineProps({
+  modelValue: String,
+});
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+  if (input.value.hasAttribute('autofocus')) {
+    input.value.focus();
+  }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+<template>
+  <div class="input-group">
+    <input ref="input" type="text" class="form-control form-control-lg" :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)" id="search_faq"
+      placeholder="Cari informasi anda disini...." />
+    <unicon name="search" class="icon_search" fill="var(--text-color)"></unicon>
+  </div>
+</template>
+
--- helpdesk_website/resources/js/app.js
+++ helpdesk_website/resources/js/app.js
@@ -32,6 +32,7 @@ import {
     uniCreateDashboard,
     uniAngleRight,
     uniBars,
+    uniSearch
 } from "vue-unicons/dist/icons";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle";
 
@@ -58,6 +59,7 @@ Unicon.add([
     uniCreateDashboard,
     uniAngleRight,
     uniBars,
+    uniSearch
 ]);
 
 createInertiaApp({
