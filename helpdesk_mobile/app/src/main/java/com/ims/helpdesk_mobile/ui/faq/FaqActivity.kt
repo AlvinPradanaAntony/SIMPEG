b5ff8bb60e8203be56da03e5a6a4c3943227ad0b
Developer <quickgaming321@gmail.com>
2023-12-29 23:54:41 +0700

    Update FAQ Layout

--- helpdesk_mobile/app/src/main/java/com/ims/helpdesk_mobile/ui/faq/FaqActivity.kt
+++ helpdesk_mobile/app/src/main/java/com/ims/helpdesk_mobile/ui/faq/FaqActivity.kt
@@ -1,7 +1,9 @@
 package com.ims.helpdesk_mobile.ui.faq
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ims.helpdesk_mobile.R
 import com.ims.helpdesk_mobile.databinding.ActivityFaqBinding
 
 class FaqActivity : AppCompatActivity() {
@@ -10,5 +12,14 @@ class FaqActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFaqBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        customShadow()
+    }
+    private fun customShadow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            binding.view.outlineAmbientShadowColor = getColor(R.color.shadowAmbientColor)
+            binding.view.outlineSpotShadowColor = getColor(R.color.shadowSpotColor)
+        } else {
+            binding.view.elevation = 6f
+        }
     }
 }
