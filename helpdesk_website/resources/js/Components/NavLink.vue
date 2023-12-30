0ec92c9fbfddea056c8f6499cd9dc522934d9fb1
Developer <quickgaming321@gmail.com>
2023-12-30 14:51:55 +0700

    fix error missing id on Navlink

--- helpdesk_website/resources/js/Components/NavLink.vue
+++ helpdesk_website/resources/js/Components/NavLink.vue
@@ -10,7 +10,8 @@ const { canLogin, auth, tickets} = defineProps([
 ]);
 
 const userTickets = computed(() => {
-  return tickets.ticket.filter(ticket => ticket.user_id_employee === auth.id);
+  const currentUserID = auth && auth.id ? auth.id : null;
+  return tickets.ticket.filter(ticket => ticket.user_id_employee === currentUserID);
 });
 
 const logout = () => {
@@ -101,7 +102,7 @@ const calculateTimeDifference = (updatedAt) => {
                 d="M19 13.586V10c0-3.217-2.185-5.927-5.145-6.742C13.562 2.52 12.846 2 12 2s-1.562.52-1.855 1.258C7.185 4.074 5 6.783 5 10v3.586l-1.707 1.707A.996.996 0 0 0 3 16v2a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L19 13.586zM19 17H5v-.586l1.707-1.707A.996.996 0 0 0 7 14v-4c0-2.757 2.243-5 5-5s5 2.243 5 5v4c0 .266.105.52.293.707L19 16.414V17zm-7 5a2.98 2.98 0 0 0 2.818-2H9.182A2.98 2.98 0 0 0 12 22z">
               </path>
             </svg>
-            <span class="position-absolute bg-danger badge rounded-pill pb-1"> {{ userTickets.length }} </span>
+            <span class="position-absolute bg-danger badge rounded-pill pb-1"> {{ auth? userTickets.length : '0' }} </span>
           </div>
         </a>
         <div class="dropdown-menu dropdown-menu-center custom-rounded border-0 notification pb-3">
@@ -114,8 +115,8 @@ const calculateTimeDifference = (updatedAt) => {
               </span>Notifikasi
             </h5>
           </div>
-          <div class="noti-body" id="simple-bar" v-for="ticket in userTickets" :key="ticket.id">
-            <!-- item-->
+          <div v-if="auth != null">
+            <div class="noti-body" id="simple-bar" v-for="ticket in userTickets" :key="ticket.id">
             <Link href="/" class="dropdown-item notify-item">
             <div class="d-flex justify-content-center" :class="getIconClass(ticket.statuses.status)">
               <unicon name="info-circle" width="24" fill="white" />
@@ -127,6 +128,7 @@ const calculateTimeDifference = (updatedAt) => {
               </small>
             </p>
             </Link>
+          </div>
           </div>
         </div>
       </li>
