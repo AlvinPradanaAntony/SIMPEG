9f48ff1ae64515d29d8afd633cd5ed7f128d19e8
Alvin Pradana Antony <quickgaming321@gmail.com>
2024-01-10 17:27:15 +0700

    Update Layout Edit

--- helpdesk_mobile/app/src/main/java/com/ims/helpdesk_mobile/ui/profile/EditProfileActivity.kt
+++ helpdesk_mobile/app/src/main/java/com/ims/helpdesk_mobile/ui/profile/EditProfileActivity.kt
@@ -1,34 +1,28 @@
 package com.ims.helpdesk_mobile.ui.profile
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.appbar.MaterialToolbar
 import com.ims.helpdesk_mobile.R
-import com.ims.helpdesk_mobile.databinding.ActivityProfileBinding
+import com.ims.helpdesk_mobile.databinding.ActivityEditProfileBinding
 
-class ProfileActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityProfileBinding
+class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityEditProfileBinding
     private lateinit var toolbar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProfileBinding.inflate(layoutInflater)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
-        setupAction()
     }
+
     private fun setupView() {
         toolbar = binding.topAppBar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(true)
     }
-    private fun setupAction(){
-        binding.buttonEdit.setOnClickListener {
-            startActivity(Intent(this@ProfileActivity, EditProfileActivity::class.java))
-        }
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
